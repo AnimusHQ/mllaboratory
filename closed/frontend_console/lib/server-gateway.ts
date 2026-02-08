16c104e702deb664c9e28648be3c5318bd8382a7
Maksim Sotnikov <m4loy.green@yandex.ru>
2026-02-08 05:39:43 +0300

    [UI][GUARD] Send project header in server fetch

--- closed/frontend_console/lib/server-gateway.ts
+++ closed/frontend_console/lib/server-gateway.ts
@@ -1,12 +1,19 @@
 import { cookies } from 'next/headers';
 
 import { gatewayFetchJSON, type GatewayFetchOptions } from '@/lib/gateway-client';
+import { getActiveProjectId } from '@/lib/server-context';
 
 export async function gatewayServerFetchJSON<T>(path: string, init: GatewayFetchOptions = {}): Promise<T> {
   const cookieHeader = cookies().toString();
   const headers = new Headers(init.headers ?? {});
   if (cookieHeader) {
     headers.set('Cookie', cookieHeader);
+  }
+  if (!headers.has('X-Project-Id') && !headers.has('X-Project-ID')) {
+    const projectId = await getActiveProjectId();
+    if (projectId) {
+      headers.set('X-Project-Id', projectId);
+    }
   }
   return gatewayFetchJSON<T>(path, {
     ...init,
